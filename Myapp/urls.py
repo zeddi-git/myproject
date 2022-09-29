658cc6b69e8feb6f038af61017a0dc328366e1cf
Zeddi Owino <80245734+zeddi-git@users.noreply.github.com>
2022-09-29 23:46:14 +0300

    Progress of login, sign-up and logout

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -5,6 +5,9 @@ from . import views  #this imports the views function
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('counter', views.counter, name= 'counter'),
-    path('register',views.register, name = 'register')
+    path('register',views.register, name = 'register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name= 'logout'),
+    path('post/<str:pk>', views.post, name= 'post')
 ]
 
--- Myapp/views.py
+++ Myapp/views.py
@@ -73,3 +73,29 @@ def register(request):
             return redirect('register')
     else:
         return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user= auth.authenticate(username=username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+
+        else:
+            messages.info(request, 'Credentials do not exist. Please sign up')
+            return redirect('register')
+        
+    else:
+        return render (request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk': pk})
+
